820718fd1eccb940dc8bdaba3f3949d32d22115d
ErfanHamdi <erfan.hamdi@gmail.com>
2019-07-06 09:34:37 +0430

    user pass changed

--- aut-login.py
+++ aut-login.py
@@ -1,6 +1,6 @@
 from webbot import Browser 
 web = Browser()
 web.go_to('https://internet.aut.ac.ir/') 
-web.type('m_erfan_hamdi' , into='username') #enter your username here !
-web.type('25HK884V5' , into='password') #Enter your password here !
+web.type('user' , into='username') #enter your username here !
+web.type('pass' , into='password') #Enter your password here !
 web.click('ورود')
